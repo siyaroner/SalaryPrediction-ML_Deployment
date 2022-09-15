07140a65297f85614a07863bbf1be948e8c9eb78
Şiyar Öner <siyaroner@gmail.com>
2022-09-15 05:27:55 +0300

    model.py created to dump model.pkl, requirements.txt updated

--- app/app.py
+++ app/app.py
@@ -1,1 +1,22 @@
-ECHO is on.
+import numpy as np
+from flask import Flask, request,jsonify,render_template
+import pickle
+
+app=Flask(__name__ )
+model=pickle.load(open("model.pkl","rb"))
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    """for rendering result on html gui
+    """
+    int_features=[int(x) for x in request.form.values()]
+    final_features=[np.array(int_features) for x in request.form.values()]
+    prediction=model.predict(final_features)
+    
+    output=round(prediction[0],2)
+    
+    return render_template("index.html",prediction_text=(f"Employee Salary should be $ {output}"))
--- app/model.py
+++ app/model.py
@@ -1,1 +1,37 @@
-ECHO is on.
+#libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle 
+
+#importing dataset
+dataset=pd.read_csv('hiring.csv')
+dataset['experience'].fillna(0,inplace=True)
+dataset['test_score'].fillna(dataset['test_score'].mean(),inplace=True)
+X=dataset.iloc[:,:3]
+
+#Converting words to integer values
+def conver_to_int(word):
+    word_dict={'one':1, 'two':2, 'three':3,'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 
+               'ten':10, 'eleven':11,'twelve':12,'zero':0, 0:0}
+    return word_dict[word]
+X['experience']=X['experience'].apply(lambda x: conver_to_int(x))
+y=dataset.iloc[:,-1]
+
+#Splitting training and test set
+#since we have a very dataset, we will train our model with all available data
+
+from sklearn.linear_model import LinearRegression
+
+regressor=LinearRegression()
+
+#fitting model with training data
+regressor.fit(X,y)
+
+#Saving model
+pickle.dump(regressor,open("model.pkl","wb"))
+
+#Loading model to compare the results
+model=pickle.load(open("model.pkl","rb"))
+print(model.predict([[2,9,7]]))
+ 
