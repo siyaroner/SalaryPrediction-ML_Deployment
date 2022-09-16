0225d0711645dc20c3be196dde2183da16aaca39
Şiyar Öner <siyaroner@gmail.com>
2022-09-16 22:29:00 +0300

    creating model.pkl and hiring.csv

--- app/model.py
+++ app/model.py
@@ -6,6 +6,7 @@ import pickle
 
 #importing dataset
 dataset=pd.read_csv('hiring.csv')
+print(dataset)
 dataset['experience'].fillna(0,inplace=True)
 dataset['test_score'].fillna(dataset['test_score'].mean(),inplace=True)
 X=dataset.iloc[:,:3]
@@ -13,7 +14,7 @@ X=dataset.iloc[:,:3]
 #Converting words to integer values
 def conver_to_int(word):
     word_dict={'one':1, 'two':2, 'three':3,'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 
-               'ten':10, 'eleven':11,'twelve':12,'zero':0, 0:0}
+                'ten':10, 'eleven':11,'twelve':12,'zero':0, 0:0}
     return word_dict[word]
 X['experience']=X['experience'].apply(lambda x: conver_to_int(x))
 y=dataset.iloc[:,-1]
